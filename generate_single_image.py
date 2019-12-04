061d0e94151e8ce2f34cb40e27083085b19d06b4
charleschiu2012 <charleschiu2012@gmail.com>
2019-12-04 16:19:50 +0800

    test p_gamma diff

--- generate_single_image.py
+++ generate_single_image.py
@@ -162,11 +162,16 @@ if __name__ == '__main__':
     glLoadIdentity()
 
     # CAMERA location
-    c_gamma, c_theta, c_phi, c_x, c_y, c_z = set_camera_position(LOWER_BOUND, UPPER_BOUND)
+    # c_gamma, c_theta, c_phi, c_x, c_y, c_z = set_camera_position(LOWER_BOUND, UPPER_BOUND)
+    c_gamma, c_theta, c_phi = 1.74308766628, math.pi * 0, 2 * math.pi * 0
+    c_x, c_y, c_z = ball_coordinates_to_cassette_coordinates(c_gamma, c_theta, c_phi)
     # WHERE does camera look at
-    p_gamma, p_theta, p_phi, p_x, p_y, p_z = set_optical_axis_look_at(MOON_RADIUS)
+    # p_gamma, p_theta, p_phi, p_x, p_y, p_z = set_optical_axis_look_at(MOON_RADIUS)
+    p_gamma, p_theta, p_phi = 1.742887, math.acos(1 - 2 * 0), 2 * math.pi * 0
+    p_x, p_y, p_z = ball_coordinates_to_cassette_coordinates(p_gamma, p_theta, p_phi)
     # DIRECTION of camera
     u_x, u_y, u_z = camera_direction(c_x, c_y, c_z, p_x, p_y, p_z)
+
     # take the shoot
     gluLookAt(c_x, c_y, c_z, p_x, p_y, p_z, u_x, u_y, u_z)
     glCallList(obj.gl_list)
