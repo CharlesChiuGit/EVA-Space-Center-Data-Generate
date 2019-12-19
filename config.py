d5d773bb1e97c69fe9e126059909c95cbdc22319
charleschiu2012 <charleschiu2012@gmail.com>
2019-12-19 22:48:42 +0800

    creat Dataset_all_random_half_p

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ import logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)-8s %(message)s', datefmt='%m-%d %H:%M:%S')
 
 # Dateset name
-DATASET_NAME = 'Dataset_all_random_2'
+DATASET_NAME = 'Dataset_all_random_half_p'
 SINGLE_IMAGE = '_61957'
 
 # Units
@@ -22,13 +22,14 @@ LOWER_BOUND = MOON_RADIUS + (OPENGL_1_METER * 200)  # 200m above moon surface
 UPPER_BOUND = MOON_RADIUS + (OPENGL_1_METER * 10000)   # 10,000m above moon surface
 
 # PATH
-PATH = "./Single_Image"
-# PATH = os.path.join('/data/', DATASET_NAME)
+# PATH = "./Single_Image"
+PATH = os.path.join('/data/', DATASET_NAME)
 if not os.path.exists(PATH):
     logging.info('Create dataset {}'.format(DATASET_NAME))
     os.makedirs(PATH)
 
 # hyperparameters
+TOTAL_IMAGE_NUM = 1000
 LEVEL_1_INDEX = 10
 LEVEL_2_INDEX = 10
-IMAGE_INDEX = 1000
+IMAGE_INDEX = (TOTAL_IMAGE_NUM/LEVEL_1_INDEX)/LEVEL_2_INDEX
