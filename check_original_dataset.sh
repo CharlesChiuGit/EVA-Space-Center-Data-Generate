90abad33db12c04bf8f6db6ed427ab6b05b23e57
charleschiu2012 <charleschiu2012@gmail.com>
2019-12-22 07:25:51 +0800

    try check_original_dataset.sh

--- check_original_dataset.sh
+++ check_original_dataset.sh
@@ -6,27 +6,28 @@ single_img_folder='/home/eva/space_center/moon_8K/Single_Image/'
 echo 'Start checking original dataset'
 
 #for i in $(seq 0 9)
-#for i in $(seq 0 )
-#do
-#  for j in $(seq 0 9)
-#  do
-#    for img in $(ls "${local_dataset_path}/$i/${i}_$j/")
-#    do
-#      pngcheck -q "${local_dataset_path}/$i/${i}_$j/$img"
-#      retval=$?
-#      if [ $retval -ne 0 ]; then
-#        echo $retval
-#      fi
-##      python generate_single_image.py -s $img
-#
-#    done
-#  done
-#done
-#
+for i in $(seq 5 )
+do
+  for j in $(seq 0 )
+  do
+    for img in $(ls "${local_dataset_path}/$i/${i}_$j/")
+    do
+      pngcheck -q "${local_dataset_path}/$i/${i}_$j/$img"
+      retval=$?
+      if [ $retval -ne 0 ]; then
+        echo "${img}"
+      fi
+#      python generate_single_image.py -s $img
+
+    done
+  done
+done
+
 #echo 'End checking original dataset'
-img="Dataset_all_random_50377.png"
-pngcheck -q "${local_dataset_path}/5/5_0/${img}"
-retval=$?
-if [ $retval -ne 0 ]; then
-  echo "${img}"
-fi
+#
+#img="Dataset_all_random_50377.png"
+#pngcheck -q "${local_dataset_path}/5/5_0/${img}"
+#retval=$?
+#if [ $retval -ne 0 ]; then
+#  echo "${img}"
+#fi
