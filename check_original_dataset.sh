a8a79b6adf75b94a81db0f16890b5687cc37be96
charleschiu2012 <charleschiu2012@gmail.com>
2019-12-22 07:57:39 +0800

    try check_original_dataset.sh

--- check_original_dataset.sh
+++ check_original_dataset.sh
@@ -16,7 +16,7 @@ do
       retval=$?
       if [ $retval -ne 0 ]; then
         echo "${img}"
-        python generate_single_image.py -s ${img} ${object}
+        python generate_single_image.py -s ${img}  ${object}
 #        python generate_single_image.py
       fi
     done
