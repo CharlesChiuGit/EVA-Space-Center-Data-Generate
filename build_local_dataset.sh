e1b76edb0b5fe11f89a6d8aff1480b9df3f27d2e
charleschiu2012 <charleschiu2012@gmail.com>
2019-12-22 19:01:28 +0800

    fix six_dataset

--- build_local_dataset.sh
+++ build_local_dataset.sh
@@ -20,59 +20,59 @@ echo 'Start building local dataset'
 #  mkdir -m 777 -v "${local_dataset_path}/${file_type[i]}/${data_type[1]}"
 #done
 
-echo 'Start decompressing'
-for i in $(seq 0 7)
-do
-  train_image_file="${local_dataset_path}/${file_type[0]}/${data_type[0]}"
-#  mkdir -m 777 -v "${train_image_file}/$i"
-  train_label_file="${local_dataset_path}/${file_type[0]}/${data_type[1]}"
-#  mv "${local_dataset_path}/target_$i.json"  "${train_label_file}"
-  echo "$i"
-  for j in $(seq 0 9)
-  do
-#    mkdir -m 777 -v "${train_image_file}/$i/${i}_$j"
-#    tar -C "${train_image_file}/$i/${i}_$j" -zxf "${local_dataset_path}/${file_type[3]}/${i}_$j.tar.gz"
-    for img in "${train_image_file}/$i/${i}_$j"/*.png
-    do
-#      echo "${train_image_file}/$i/${i}_$j/$img"
-      pngcheck -q "$img"
-    done
-  done
-done
-
-test_image_file="${local_dataset_path}/${file_type[1]}/${data_type[0]}"
-#mkdir -m 777 -v "${test_image_file}/0"
-test_label_file="${local_dataset_path}/${file_type[1]}/${data_type[1]}"
-#mv "${local_dataset_path}/target_8.json"  "${test_label_file}"
-echo "8"
-for j in $(seq 0 9)
-do
-#  mkdir -m 777 -v "${test_image_file}/0/0_$j"
-#  tar -C "${test_image_file}/0/0_$j" -zxf "${local_dataset_path}/${file_type[3]}/8_$j.tar.gz"
-  for img in "${test_image_file}/0/0_$j"/*.png
-  do
-#    echo "${test_image_file}/0/0_$j/$img"
-    pngcheck -q "$img"
-  done
-done
-
-validation_image_file="${local_dataset_path}/${file_type[2]}/${data_type[0]}"
-#mkdir -m 777 -v "${validation_image_file}/0"
-validation_label_file="${local_dataset_path}/${file_type[2]}/${data_type[1]}"
-#mv "${local_dataset_path}/target_9.json"  "${validation_label_file}"
-echo "9"
-for j in $(seq 0 9)
-do
-#  mkdir -m 777 -v "${validation_image_file}/0/0_$j"
-#  tar -C "${validation_image_file}/0/0_$j" -zxf "${local_dataset_path}/${file_type[3]}/9_$j.tar.gz"
-  for img in "${validation_image_file}/0/0_$j"/*.png
-  do
-#    echo "${validation_image_file}/0/0_$j/$img"
-    pngcheck -q "$img"
-  done
-done
-echo 'End decompressing'
-echo 'End building local dataset'
+#echo 'Start decompressing'
+#for i in $(seq 0 7)
+#do
+#  train_image_file="${local_dataset_path}/${file_type[0]}/${data_type[0]}"
+##  mkdir -m 777 -v "${train_image_file}/$i"
+#  train_label_file="${local_dataset_path}/${file_type[0]}/${data_type[1]}"
+##  mv "${local_dataset_path}/target_$i.json"  "${train_label_file}"
+#  echo "$i"
+#  for j in $(seq 0 9)
+#  do
+##    mkdir -m 777 -v "${train_image_file}/$i/${i}_$j"
+##    tar -C "${train_image_file}/$i/${i}_$j" -zxf "${local_dataset_path}/${file_type[3]}/${i}_$j.tar.gz"
+#    for img in "${train_image_file}/$i/${i}_$j"/*.png
+#    do
+##      echo "${train_image_file}/$i/${i}_$j/$img"
+#      pngcheck -q "$img"
+#    done
+#  done
+#done
+#
+#test_image_file="${local_dataset_path}/${file_type[1]}/${data_type[0]}"
+##mkdir -m 777 -v "${test_image_file}/0"
+#test_label_file="${local_dataset_path}/${file_type[1]}/${data_type[1]}"
+##mv "${local_dataset_path}/target_8.json"  "${test_label_file}"
+#echo "8"
+#for j in $(seq 0 9)
+#do
+##  mkdir -m 777 -v "${test_image_file}/0/0_$j"
+##  tar -C "${test_image_file}/0/0_$j" -zxf "${local_dataset_path}/${file_type[3]}/8_$j.tar.gz"
+#  for img in "${test_image_file}/0/0_$j"/*.png
+#  do
+##    echo "${test_image_file}/0/0_$j/$img"
+#    pngcheck -q "$img"
+#  done
+#done
+#
+#validation_image_file="${local_dataset_path}/${file_type[2]}/${data_type[0]}"
+##mkdir -m 777 -v "${validation_image_file}/0"
+#validation_label_file="${local_dataset_path}/${file_type[2]}/${data_type[1]}"
+##mv "${local_dataset_path}/target_9.json"  "${validation_label_file}"
+#echo "9"
+#for j in $(seq 0 9)
+#do
+##  mkdir -m 777 -v "${validation_image_file}/0/0_$j"
+##  tar -C "${validation_image_file}/0/0_$j" -zxf "${local_dataset_path}/${file_type[3]}/9_$j.tar.gz"
+#  for img in "${validation_image_file}/0/0_$j"/*.png
+#  do
+##    echo "${validation_image_file}/0/0_$j/$img"
+#    pngcheck -q "$img"
+#  done
+#done
+#echo 'End decompressing'
+#echo 'End building local dataset'
 
 #echo 'Start checking dataset'
 #image_file="${local_dataset_path}/${file_type[1]}/${data_type[0]}"
@@ -85,15 +85,15 @@ echo 'End building local dataset'
 #    done
 #done
 #echo 'End checking dataset'
-
-#tar -C "${image_file}/0/0_0" -zxf "${local_dataset_path}/compressed_file/0_0.tar.gz"
-#tar -C "${image_file}/2/2_8" -zxf "${local_dataset_path}/compressed_file/2_8.tar.gz"
+train_image_file="${local_dataset_path}/${file_type[0]}/${data_type[0]}"
+tar -C "${train_image_file}/4/4_6" -zxf "${local_dataset_path}/compressed_file/4_6.tar.gz"
+tar -C "${train_image_file}/7/7_6" -zxf "${local_dataset_path}/compressed_file/7_6.tar.gz"
 #tar -C "${image_file}/4/4_5" -zxf "${local_dataset_path}/compressed_file/4_5.tar.gz"
 #tar -C "${image_file}/6/6_3" -zxf "${local_dataset_path}/compressed_file/6_3.tar.gz"
 #tar -C "${image_file}/6/6_4" -zxf "${local_dataset_path}/compressed_file/6_4.tar.gz"
-#echo 'End decompressing'
-#pngcheck -q "/data/space/Dataset_six_random/train/images/0/0_0/Dataset_six_random_75.png"
-#pngcheck -q "/data/space/Dataset_six_random/train/images/2/2_8/Dataset_six_random_56129.png"
+echo 'End decompressing'
+pngcheck -q "/data/space/Dataset_six_random/train/images/4/4_6/Dataset_six_random_92664.png"
+pngcheck -q "/data/space/Dataset_six_random/train/images/7/7_6/Dataset_six_random_152692.png"
 #pngcheck -q "/data/space/Dataset_six_random/train/images/4/4_5/Dataset_six_random_90538.png"
 #pngcheck -q "/data/space/Dataset_six_random/train/images/6/6_3/Dataset_six_random_127609.png"
 #pngcheck -q "/data/space/Dataset_six_random/train/images/6/6_4/Dataset_six_random_128244.png"
