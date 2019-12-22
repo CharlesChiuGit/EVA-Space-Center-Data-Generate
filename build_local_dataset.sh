e24296420acd1c86182a18cc32c5864f654493df
charleschiu2012 <charleschiu2012@gmail.com>
2019-12-22 18:09:00 +0800

    fix build_local_dataset.sh

--- build_local_dataset.sh
+++ build_local_dataset.sh
@@ -8,64 +8,64 @@ file_type=('train' 'test' 'validation' 'compressed_file')
 data_type=('images' 'labels')
 
 echo 'Start building local dataset'
-mkdir -m 777 -v "${local_dataset_path}"
-mkdir -m 777 -v "${local_dataset_path}/${file_type[3]}"
-scp -i ${local_private_key} ${remote_IP}:${remote_dataset_path}/*.tar.gz "${local_dataset_path}/${file_type[3]}"
-scp -i ${local_private_key} ${remote_IP}:${remote_dataset_path}/tar* "${local_dataset_path}"
+#mkdir -m 777 -v "${local_dataset_path}"
+#mkdir -m 777 -v "${local_dataset_path}/${file_type[3]}"
+#scp -i ${local_private_key} ${remote_IP}:${remote_dataset_path}/*.tar.gz "${local_dataset_path}/${file_type[3]}"
+#scp -i ${local_private_key} ${remote_IP}:${remote_dataset_path}/tar* "${local_dataset_path}"
 
-for i in $(seq 0 2)
-do
-  mkdir -m 777 -v "${local_dataset_path}/${file_type[i]}"
-  mkdir -m 777 -v "${local_dataset_path}/${file_type[i]}/${data_type[0]}"
-  mkdir -m 777 -v "${local_dataset_path}/${file_type[i]}/${data_type[1]}"
-done
+#for i in $(seq 0 2)
+#do
+#  mkdir -m 777 -v "${local_dataset_path}/${file_type[i]}"
+#  mkdir -m 777 -v "${local_dataset_path}/${file_type[i]}/${data_type[0]}"
+#  mkdir -m 777 -v "${local_dataset_path}/${file_type[i]}/${data_type[1]}"
+#done
 
 echo 'Start decompressing'
 for i in $(seq 0 7)
 do
   train_image_file="${local_dataset_path}/${file_type[0]}/${data_type[0]}"
-  mkdir -m 777 -v "${train_image_file}/$i"
+#  mkdir -m 777 -v "${train_image_file}/$i"
   train_label_file="${local_dataset_path}/${file_type[0]}/${data_type[1]}"
-  mv "${local_dataset_path}/target_$i.json"  "${train_label_file}"
+#  mv "${local_dataset_path}/target_$i.json"  "${train_label_file}"
   for j in $(seq 0 9)
   do
-    mkdir -m 777 -v "${train_image_file}/$i/${i}_$j"
-    tar -C "${train_image_file}/$i/${i}_$j" -zxf "${local_dataset_path}/${file_type[3]}/${i}_$j.tar.gz"
+#    mkdir -m 777 -v "${train_image_file}/$i/${i}_$j"
+#    tar -C "${train_image_file}/$i/${i}_$j" -zxf "${local_dataset_path}/${file_type[3]}/${i}_$j.tar.gz"
     for img in "${train_image_file}/$i/${i}_$j"/*.png
     do
 #      echo "${train_image_file}/$i/${i}_$j/$img"
-      pngcheck -q "${train_image_file}/$i/${i}_$j/$img"
+      pngcheck -q "$img"
     done
   done
 done
 
 test_image_file="${local_dataset_path}/${file_type[1]}/${data_type[0]}"
-mkdir -m 777 -v "${test_image_file}/0"
+#mkdir -m 777 -v "${test_image_file}/0"
 test_label_file="${local_dataset_path}/${file_type[1]}/${data_type[1]}"
-mv "${local_dataset_path}/target_8.json"  "${test_label_file}"
+#mv "${local_dataset_path}/target_8.json"  "${test_label_file}"
 for j in $(seq 0 9)
 do
-  mkdir -m 777 -v "${test_image_file}/0/0_$j"
-  tar -C "${test_image_file}/0/0_$j" -zxf "${local_dataset_path}/${file_type[3]}/8_$j.tar.gz"
+#  mkdir -m 777 -v "${test_image_file}/0/0_$j"
+#  tar -C "${test_image_file}/0/0_$j" -zxf "${local_dataset_path}/${file_type[3]}/8_$j.tar.gz"
   for img in "${test_image_file}/0/0_$j"/*.png
   do
 #    echo "${test_image_file}/0/0_$j/$img"
-    pngcheck -q "${test_image_file}/0/0_$j/$img"
+    pngcheck -q "$img"
   done
 done
 
 validation_image_file="${local_dataset_path}/${file_type[2]}/${data_type[0]}"
-mkdir -m 777 -v "${validation_image_file}/0"
+#mkdir -m 777 -v "${validation_image_file}/0"
 validation_label_file="${local_dataset_path}/${file_type[2]}/${data_type[1]}"
-mv "${local_dataset_path}/target_9.json"  "${validation_label_file}"
+#mv "${local_dataset_path}/target_9.json"  "${validation_label_file}"
 for j in $(seq 0 9)
 do
-  mkdir -m 777 -v "${validation_image_file}/0/0_$j"
-  tar -C "${validation_image_file}/0/0_$j" -zxf "${local_dataset_path}/${file_type[3]}/9_$j.tar.gz"
+#  mkdir -m 777 -v "${validation_image_file}/0/0_$j"
+#  tar -C "${validation_image_file}/0/0_$j" -zxf "${local_dataset_path}/${file_type[3]}/9_$j.tar.gz"
   for img in "${validation_image_file}/0/0_$j"/*.png
   do
 #    echo "${validation_image_file}/0/0_$j/$img"
-    pngcheck -q "${validation_image_file}/0/0_$j/$img"
+    pngcheck -q "$img"
   done
 done
 echo 'End decompressing'
