a55929570572daae508214c8d102cb8d899792da
charleschiu2012 <charleschiu2012@gmail.com>
2019-12-22 22:10:29 +0800

    check 51643 target

--- replace_target.py
+++ replace_target.py
@@ -17,39 +17,13 @@ def read_json(file_path):
 
 
 if __name__ == '__main__':
-    # local_dataset_path = '/data/' + DATASET_NAME
-    # new_labels_path = os.path.join(PATCH_PATH, 'target_' + DATASET_NAME + '_*.json')
-    # new_label_files = sorted(glob(new_labels_path))
-    # old_label_path = os.path.join(local_dataset_path, 'target_' + sys.argv[2] + '.json')
-    # print(new_label_files)
-    # print(old_label_path)
-    # keys, datas = read_json(old_label_path)
-    # for i in range(len(new_label_files)):
-    #     [new_key], new_data = read_json(new_label_files[i])
-    #     true_new_key = remove_filename_extension(sys.argv[1])
-    #     if true_new_key == new_key:
-    #         print('New data: ', new_data[new_key])
-    #         for key in keys:
-    #             if key == new_key:
-    #                 datas[new_key] = new_data[new_key]
-    #     else:
-    #         logging.error('keys doesnot match!')
-    # print('Renew data: ', datas[new_key])
-    # with open(old_label_path, 'w') as f:
-    #     json.dump(datas, f)
-    #     logging.info('Finish saving {}'.format(old_label_path))
-
-    # sys.argv[2] = '2'
-    local_dataset_path = '/data/space/' + DATASET_NAME + '/train/labels/'
-    PATCH_PATH = '/home/charleschiu/'
+    local_dataset_path = '/data/' + DATASET_NAME
     new_labels_path = os.path.join(PATCH_PATH, 'target_' + DATASET_NAME + '_*.json')
     new_label_files = sorted(glob(new_labels_path))
     old_label_path = os.path.join(local_dataset_path, 'target_' + sys.argv[2] + '.json')
-    # sys.argv[1] = 'Dataset_six_random_51643.png'
     print(new_label_files)
     print(old_label_path)
     keys, datas = read_json(old_label_path)
-    print('Old data: ', datas[remove_filename_extension(sys.argv[1])])
     for i in range(len(new_label_files)):
         [new_key], new_data = read_json(new_label_files[i])
         true_new_key = remove_filename_extension(sys.argv[1])
@@ -57,11 +31,33 @@ if __name__ == '__main__':
             print('New data: ', new_data[new_key])
             for key in keys:
                 if key == new_key:
-                    print(new_key)
                     datas[new_key] = new_data[new_key]
         else:
             logging.error('keys doesnot match!')
-    # print('Renew data: ', datas[new_key])
+    print('Renew data: ', datas[new_key])
+    with open(old_label_path, 'w') as f:
+        json.dump(datas, f)
+        logging.info('Finish saving {}'.format(old_label_path))
 
-
-
+    # #  check 58 target if true replaced
+    # local_dataset_path = '/data/space/' + DATASET_NAME + '/train/labels/'
+    # PATCH_PATH = '/home/charleschiu/'
+    # new_labels_path = os.path.join(PATCH_PATH, 'target_' + DATASET_NAME + '_*.json')
+    # new_label_files = sorted(glob(new_labels_path))
+    # old_label_path = os.path.join(local_dataset_path, 'target_' + sys.argv[2] + '.json')
+    # print(new_label_files)
+    # print(old_label_path)
+    # keys, datas = read_json(old_label_path)
+    # print('Old data: ', datas[remove_filename_extension(sys.argv[1])])
+    # for i in range(len(new_label_files)):
+    #     [new_key], new_data = read_json(new_label_files[i])
+    #     true_new_key = remove_filename_extension(sys.argv[1])
+    #     if true_new_key == new_key:
+    #         print('New data: ', new_data[new_key])
+    #         for key in keys:
+    #             if key == new_key:
+    #                 print(new_key)
+    #                 datas[new_key] = new_data[new_key]
+    #                 logging.info('target match!')
+    #     else:
+    #         logging.error('keys doesnot match!')
